d5f3d2de11b3cbc99b1b7329a8df98e0d9d4f085
Ferdiansyah <ferdiansyah.fr42@gmail.com>
2024-11-12 18:47:23 +0700

    mengambil data yg di submid

--- script.js
+++ script.js
@@ -2,4 +2,8 @@
 document.getElementById('attendance-form').addEventListener('submit', function(event) {
     event.preventDefault();
 
+//mengambil data dr formulir
+    const name = document.getElementById('name').value;
+    const date = document.getElementById('date').value;
+    const status = document.getElementById('status').value;
 })
