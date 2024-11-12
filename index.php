37ba15298557a2076a0c10dede01c884a4d63d10
Ferdiansyah <ferdiansyah.fr42@gmail.com>
2024-11-12 19:15:36 +0700

    menambahkan required

--- index.php
+++ index.php
@@ -27,7 +27,7 @@
             </div>
             <div class="form-group">
                 <label for="status">Status</label>
-                <select id="status">
+                <select id="status" required>
                     <option value="hadir">Hadir</option>
                     <option value="izin">Izin</option>
                     <option value="alpa">Alpa</option>
