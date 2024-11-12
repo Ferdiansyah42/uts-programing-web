b8c86095b9ac8b5b200708ceb6f1632c3b110e34
Ferdiansyah <ferdiansyah.fr42@gmail.com>
2024-11-12 19:03:32 +0700

    membuat agar data yng di kirim masuk ke tabel

--- script.js
+++ script.js
@@ -6,4 +6,9 @@ document.getElementById('attendance-form').addEventListener('submit', function(e
     const name = document.getElementById('name').value;
     const date = document.getElementById('date').value;
     const status = document.getElementById('status').value;
+
+// memasukan data ke tabel
+    const tabel = document.getElementById('attendance-tabel').getElementsByTagName('tbody')[0];
+    const newRow = table.insertRow();
+
 })
