48f5fb563b54fc407c63015762e44b216c34aeb5
Ferdiansyah <ferdiansyah.fr42@gmail.com>
2024-11-12 13:32:55 +0700

    menambahkna program php

--- index.php
+++ index.php
@@ -45,6 +45,18 @@
                     <th>Status kehadiran</th>
                 </tr>
             </thead>
+            <tbody>
+                <!-- php -->
+                <?php
+                if (inset($_GET['name']) && inset($_GET['date']) && inset($_GET['status'])) {
+                    $name = htmlspecialchars($_GET['name']);
+                    $date = htmlspecialchars($_GET['date']);
+                    $status = htmlspecialchars($_GET['status']);
+
+                    echo "<tr><td>{$name}</td><td>{$date}</td><td>{$status}</td></tr>";
+                }
+                ?>
+            </tbody>
         </table>
     </section>
 </body>
